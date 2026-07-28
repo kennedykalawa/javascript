11607ede32afa6afe2c7bd3ed8f4f80baae77531
kennedykalawa <kalawamkennedy@gmail.com>
2026-07-28 06:01:49 +0300

    variables

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # javascript
+with const and let 
+use let only when you know you can reassign the value 
+to keep code clean use "const"
--- intro.js
+++ intro.js
@@ -1,4 +1,5 @@
-console.log("Hello Ktec!");
+// you can use var, let, const to assign variables
+/*console.log("Hello Ktec!");
 let a, b, c;
 a = 5;
 b = 10;
@@ -7,4 +8,25 @@ b = (c*a);
 
 console.log(c);
 console.log(b);
+*/ 
+// Strings Number Boolean Null Undefined
 
+const name = "John";
+const age = 20;
+const rating = 7.5;
+const iscool = true;
+const x = null;
+const y = undefined;
+let z;
+
+//console.log(typeof iscool);
+
+//Concatenation
+ // the old way
+console.log("My name is " + name + "and I am " + age );
+ /* new way or template String
+  * here we use back ticks (``) 
+  * and also you can assign it as a */
+console.log(`My name is ${name} and I am ${age}`);
+const hello = (`My name is ${name} and I am ${age}`);
+console.log(hello);
