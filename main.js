576d16cadd26898e2ead0979fb57145596720c91
kennedykalawa <kalawamkennedy@gmail.com>
2026-08-03 11:04:09 +0300

    Object Literals

--- main.js
+++ main.js
@@ -8,8 +8,54 @@ fruits.push('pears');
 fruits.unshift('strawberries');
 fruits.pop();
 
-console.log(Array.isArray("hello"));
+//console.log(Array.isArray("hello"));
 
-console.log(fruits.indexOf("oranges"));
+//console.log(fruits.indexOf("oranges"));
 
-console.log(fruits);
+//console.log(fruits);
+
+
+
+//OBJECT LITERALS
+const person = {
+     firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+//console.log(person.firstName, person.lastName);
+
+/*const{ firstName, lastName, address: { city }} = person;
+
+console.log(firstName);
+console.log(city);
+*/
+
+const todos = [
+    {
+        id: 1,
+        text: "Take out trash",
+        isCompleted: true 
+    },
+    {
+        id: 2,
+        text: "Meeting with boss",
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: "Dentist appt",
+        isCompleted: false 
+    }
+];
+
+console.log(todos[1]);
+
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
